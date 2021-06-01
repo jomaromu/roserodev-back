6a17298e482e80c7eb0c47af09bc28321655d631
jomaromu <jomaromu2@gmail.com>
2021-06-01 17:06:16 -0500

    correo terminado

---
+++ routes/correo.ts
@@ -0,0 +1,60 @@
+import { Router, Request, Response } from 'express';
+import { createTransport } from 'nodemailer';
+
+// instanciar el servidor
+const rutaCorreo = Router();
+
+rutaCorreo.get('/', (req: Request, resp: Response) => {
+    resp.json({
+        ok: 'ok'
+    });
+});
+
+
+rutaCorreo.post('/', (req: Request, resp: Response) => {
+
+    const objetoCorreo = {
+        from: req.body.correo,
+        nombre: req.body.nombre,
+        mensaje: req.body.mensaje
+    }
+
+    const transport = createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'jroserodevpa@gmail.com',
+            pass: '12345678Mm&',
+        }
+    });
+
+    const mailOptions = {
+        from: `roserodev ${objetoCorreo.from}`,
+        to: 'jroserodev@gmail.com',
+        subject: `Consulta desde roserodev`,
+        text: 'algo',
+        html:
+            `<h3>${objetoCorreo.nombre}</h3>
+            <h3>${objetoCorreo.from}</h3>
+            <h3>${objetoCorreo.mensaje}</h3>
+            `
+    }
+
+    transport.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            console.log(err);
+            resp.json({
+                ok: false,
+                mensaje: 'Correo no enviado'
+            });
+        } else {
+            console.log(info);
+            resp.json({
+                ok: true,
+                mensaje: 'Correo enviado'
+            });
+        }
+    });
+});
+
+export default rutaCorreo;
+
